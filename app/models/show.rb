980e7e60412795c0eb297c5907e6a3ffdfd92b06
Cody Frank <cody.frank30@gmail.com>
2019-07-06 19:30:49 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  # def build_show(x)
+  #
+  # end
+  #
+  # def build_network(x)
+  #   self.network.call_letters = x[:call_letters]
+  # end
 end
