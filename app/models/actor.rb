a9aa8fdcf76a035431d36f59db8f376dd7a73140
aparkening <aaron.parkening@gmail.com>
2019-07-10 17:48:04 -0400

    Done. Actor, Character, Network, and Show talk to each other and database.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters 
+  has_many :networks, through: :shows 
+
+  # Return actor's first and last name
+  def full_name
+    full_name = self.first_name + " " + self.last_name
+  end
+
+  # Return array of actor's characters
+  def list_roles
+    self.characters.map { |character| "#{character.name} - #{character.show.name}"}
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,22 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show 
+
+  # Return character's catchphrase
+  def say_that_thing_you_say
+    return "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  # Return associated show
+  def build_show(attributes)
+    show = Show.find_or_create_by(attributes)
+    self.show = show
+  end
+
+  # Return associated network
+  def build_network(attributes)
+    network = Network.find_or_create_by(attrib utes)
+    self.network = network
+  end
+
 end
