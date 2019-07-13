7875a330200246df18b9620c504c3913a13ddeef
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-07-13 14:56:33 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name 
+    #binding.pry
+    self.first_name + " " + self.last_name
+  end  
+  
+  def list_roles
+    self.characters.each do |c|
+      #binding.pry
+     return  c.name + " - "+c.show.name
+    end  
+    #binding.pry
+  end  
 end
