7bea53ef31b945f92d877311dd7d0353d73d92d7
earthctzn <earthctzn1@gmail.com>
2019-07-13 17:04:40 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+    def say_that_thing_you_say
+        return "#{self.name} always says: #{self.catchphrase}"
+    end
+
+    def build_network(letters)
+        self.shows.call_letters = letters
+    end
+
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(letters)
+    self.network.call_letters = letters
+  end
 end
